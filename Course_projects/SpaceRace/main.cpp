d68ce14a1edde7a168ff4c0fb925a6e756d9f8a0
Syuzanna <cd@WIN-77VPSCKFBG6>
2024-02-23 12:52:18 +0400

    ---

--- Course_projects/SpaceRace/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "SpaceRace/SpaceRace.hpp"
-
-int main()
-{
-    SpaceRace game;
-    game.run();
-    
-    return 0;
-}
--- Course_projects/SpaceRace/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -Wall -g main.cpp $(find SpaceRace -type f -iregex ".*\.cpp") -lncurses
